530a9c5c32210e37f2abd0bde02579e9c5c7a6f0
weiqiang.lin <weiqiang.lin@ximalaya.com>
2021-03-09 19:33:56 +0800

    项目初始化

---
+++ mvc/src/test/java/cn/catgod/mvc/MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.catgod.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
